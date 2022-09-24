3fd36f03e0ea1dbb50f5d0dbc4cd96fa321cf050
gHasPerT1 <113139819+gHasPerT1@users.noreply.github.com>
2022-09-23 23:34:41 -0500

    Add files via upload

---
+++ como.js
@@ -0,0 +1,21 @@
+const body = document.querySelector('body')
+
+const rain = () => {
+    let j = 0
+    while(j <= 80){
+        let gout = document.createElement('i')
+        let x = innerWidth * Math.random()
+        let time = 1 * Math.random()
+
+        gout.style.animationDuration = time <= 0.4 ? (time + 0.4) + 's' : time + 's'
+        gout.style.animationDelay = time + 's'
+        gout.style.left = x + 'px'
+
+
+        body.appendChild(gout)
+
+        j++
+    }
+
+}
+rain()
